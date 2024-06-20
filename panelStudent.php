62b28181e7671c050b94e78a49b094b9d6505581
Kevin Riva <leokevinr@gmail.com>
2024-06-19 21:46:12 -0300

    18/6

--- panelStudent.php
+++ panelStudent.php
@@ -6,22 +6,22 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Panel de estudiantes</h1>;
-    <table>;
-        <thead>;
-            <tr>;
-                <th>ID Estudiante</th>;
-                <th>Apellido</th>;
-                <th>Nombre</th>;
-                <th>DNI</th>;
-                <th>Fecha Nac.</th>;
-                <th>Tel&eacute;fono</th>;
-                <th>Acciones</th>;
-            </tr>;
-        </thead>;
-        <tbody>;
+    <h1>Panel de estudiantes</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>ID Estudiante</th>
+                <th>Apellido</th>
+                <th>Nombre</th>
+                <th>DNI</th>
+                <th>Fecha Nac.</th>
+                <th>Tel&eacute;fono</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
             <?php
-            include("Model/student.class.php");
+            include("model/student.class.php");
             $student= new Student();
             $allStudents=$student->getAllStudents();
             if($allStudents){
@@ -33,14 +33,13 @@
                     print"<td>".$data["dni"]."</td>";
                     print"<td>".$data["birthday"]."</td>";
                     print"<td>".$data["phone"]."</td>";
-                    print"<td>Los 2 botones</td>";
+                    print "<td><input type='button' name='editar' value='Editar' onclick=\"location.href='formEditStudent.php?idStudent=".$data["idStudent"]."';\" /></td>";
+                    //print"<td><input type='button' name='eliminar' value='Eliminar'/></td>";
                     print"</tr>";
                 }
-
             }else{
                 print"No existe ningun estudiante";
             }
-
             ?>
         </tbody>
     </table>
